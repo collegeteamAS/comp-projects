3f53fd302eb34fc2117a4cd5f4e495c7eaa3b805
suhhmin <h_suh@u.pacific.edu>
2014-09-23 23:47:18 -0700

    edit2
    s

--- Project3/Project3/item.h
+++ Project3/Project3/item.h
@@ -7,30 +7,31 @@
 #define _ITEM_H_INCLUDED
 
 
-class Item{
-protected:
-	// location on Location Board
-	int xBoard;
-	int yBoard;
+class Item
+{
+	protected:
+		// location on Location Board
+		int xBoard;
+		int yBoard;
 
-	// location in room
-	int xRoom;
-	int yRoom;
+		// location in room
+		int xRoom;
+		int yRoom;
 
-public:
-	char symbol;
-	bool keyAcheived;
-	std::string description;
+	public:
+		char symbol;
+		bool keyAcheived;
+		std::string description;
 
-	Item();
-	Item(char sym);
+		Item();
+		Item(char sym);
 
-	virtual void action();
+		virtual void action();
 
-	char getSymbol();
-	int getBoardLocX();
-	int getBoardLocY();
-	int getItemLocX();
-	int getItemLocY();
+		char getSymbol();
+		int getBoardLocX();
+		int getBoardLocY();
+		int getItemLocX();
+		int getItemLocY();
 };
 #endif
